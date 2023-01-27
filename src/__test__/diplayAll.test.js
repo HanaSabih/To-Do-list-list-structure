00a8e744579a3cca00b29750ba5ac705ca30eb33
hana <hanasabih@gmail.com>
2023-01-27 12:36:54 +0200

    test checked and unchecked tasks

--- src/__test__/diplayAll.test.js
+++ src/__test__/diplayAll.test.js
@@ -2,45 +2,110 @@
  * @jest-environment jsdom
  */
 
-import { saveTodo, displayToDo } from '../modules/diplayAll.js';
+import {
+  saveTodo,
+  displayToDo,
+  deleteOneItem,
+  checkedItems,
+  uncheckedItems,
+} from "../modules/diplayAll.js";
 
-describe('add to List , all has to be completed false and starts with index 0', () => {
+describe("add to List , all has to be completed false and starts with index 0", () => {
   test("add a task 'Task1' to local storage and check if it exists on local storage", () => {
     // Arrange
     localStorage.clear();
     // Act
-    saveTodo('hana');
+    saveTodo("hana");
     // Assert
-    expect(localStorage.getItem('ToDo')).toBe(
-      JSON.stringify([{ index: 0, description: 'hana', completed: false }]),
+    expect(localStorage.getItem("ToDo")).toBe(
+      JSON.stringify([{ index: 0, description: "hana", completed: false }])
     );
   });
   test("add a task 'Task2' to local storage and check if it exists on local storage", () => {
     // Act
-    saveTodo('salim');
+    saveTodo("salim");
     // Assert
-    expect(localStorage.getItem('ToDo')).toContain(
-      JSON.stringify({ index: 1, description: 'salim', completed: false }),
+    expect(localStorage.getItem("ToDo")).toContain(
+      JSON.stringify({ index: 1, description: "salim", completed: false })
     );
   });
   test("add a task 'Task3' to local storage and check if it exists on local storage", () => {
     // Act
-    saveTodo('hayat');
+    saveTodo("hayat");
     // Assert
-    expect(localStorage.getItem('ToDo')).toContain(
-      JSON.stringify({ index: 2, description: 'hayat', completed: false }),
+    expect(localStorage.getItem("ToDo")).toContain(
+      JSON.stringify({ index: 2, description: "hayat", completed: false })
     );
   });
 });
 
-describe('Test add to DOM function', () => {
-  test('Adding 3 li element to ul', () => {
+describe("Test add to DOM function", () => {
+  test("Adding 3 li element to ul", () => {
     // Arrange
     document.body.innerHTML = "<ul class='toDoList'></ul>";
     displayToDo();
     // Act
-    const items = document.querySelectorAll('li');
+    const items = document.querySelectorAll("li");
     // Assert
     expect(items).toHaveLength(3);
+  });
+});
+
+describe("Remove one item from list", () => {
+  test("remove the task2 from the list and check if it's length is equal to 2 and if the index is updated", () => {
+    // Arrange
+    const id = 1;
+
+    // Act
+    deleteOneItem(id);
+    // Assert
+    expect(localStorage.getItem("ToDo")).toBe(
+      '[{"index":0,"description":"hana","completed":false},{"index":1,"description":"hayat","completed":false}]'
+    );
+  });
+});
+
+describe("Test after removing from localStorage remove from  DOM function", () => {
+  test("remove 1 li element to ul", () => {
+    // Arrange
+    document.body.innerHTML = "<ul class='toDoList'></ul>";
+    displayToDo();
+    // Act
+    const items = document.querySelectorAll("li");
+    // Assert
+    expect(items).toHaveLength(2);
+  });
+});
+
+describe("update checked completed status", () => {
+  test("checked and change the completed status of task index 0 from false to true and check if it changes on local storage", () => {
+    // Act
+    checkedItems(0);
+
+    // Assert
+    expect(localStorage.getItem("ToDo")).toContain(
+      JSON.stringify({ index: 0, description: "hana", completed: true })
+    );
+  });
+  test("checked and change the completed status of task index 1 from false to true and check if it changes on local storage", () => {
+    // Act
+    checkedItems(1);
+
+    // Assert
+    expect(localStorage.getItem("ToDo")).toContain(
+      JSON.stringify({ index: 1, description: "hayat", completed: true })
+    );
+  });
+});
+
+describe("update unchecked completed status after being checked", () => {
+  test("unchecked and change the completed status of task index 1 from true to false and check if it changes on local storage", () => {
+    // Act
+    uncheckedItems(1);
+
+    // Assert
+    expect(localStorage.getItem("ToDo")).toContain(
+      JSON.stringify({ index: 1, description: "hayat", completed: false })
+    );
   });
 });
